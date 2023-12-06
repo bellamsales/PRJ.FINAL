f6468580f12a56aed269317fea24f6806e86aa2d
unknown <aluno@p0110.etecaf.local>
2023-12-06 16:45:45 -0300

    ta ino

---
+++ 37277/PRJ_FINAL37277/PRJ_FINAL37277/FRM_TUDO37277.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRJ_FINAL37277
+{
+    public partial class FRM_TUDO37277 : Form
+    {
+        public FRM_TUDO37277()
+        {
+            InitializeComponent();
+        }
+    }
+}
